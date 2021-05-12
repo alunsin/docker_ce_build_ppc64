cd47b2c611d3a6dfb8ee6777f272b4a1cac40500
Aurelien Lun-Sin <79600099+alunsin@users.noreply.github.com>
2021-05-12 19:53:52 +0200

    initial version from Lilian

---
+++ build_containerd.sh
@@ -0,0 +1,29 @@
+#/bin/bash
+
+###
+#TODO
+
+
+git clone https://github.com/docker/containerd-packaging.git
+cd containerd-packaging
+
+
+if [ $# -eq 0 ]
+then
+	TAG=$(git ls-remote  --refs  --tags https://github.com/containerd/containerd.git | cut --delimiter='/' --fields=3 | sort --version-sort| tail --lines=1)
+else
+	TAG=$1
+fi
+
+export DEBS="ubuntu:xenial ubuntu:bionic ubuntu:focal"
+export RPMS="centos:7 centos:8 fedora:31 fedora:32"
+
+
+export DISTROS="$DEBS $RPMS"
+
+for DISTRO in $DISTROS
+do
+	make REF=${TAG} docker.io/library/$DISTRO
+done
+
+
